0bc88341729cfdd6af7da00df42f707d1128f9ba
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-08 10:55:34 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+	puts( "Programming is like building a multilingual puzzle\n" );
+}
+// end function main
