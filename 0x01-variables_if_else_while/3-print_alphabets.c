931ee000dee9e2ca1267814dff7bb9c6261d9cf9
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-09 16:14:33 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,10 +13,10 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	for (ch = 'A'; ch = 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-	puchar('\n')
+	putchar('\n');
 		return (0);
 }
