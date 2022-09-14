3f366d93c2435e45b26ae55c4dacb354c2ad37f5
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-14 08:00:18 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: integer with sign
+ *
+ * Return: 0 (success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
