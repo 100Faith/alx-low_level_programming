d7188886787d2e71310aea94ff323db0dbb64408
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-14 09:28:26 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: ineger number
+ *
+ * Return: last value of digit
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (n < 0)
+	{
+		last *= -1;
+	}
+	_putchar(last + '0');
+		return (last);
+}
