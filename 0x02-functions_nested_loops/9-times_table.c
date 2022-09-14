31813d61f83d754e5834c209cf91b1687a3c2bb5
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-14 15:11:42 -0700

    Time tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,11 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			op = a * b;
-			_putchar(44);
 			_putchar(32);
+			_putchar(44);
 			if (op <= 9)
 			{
 				_putchar(32);
