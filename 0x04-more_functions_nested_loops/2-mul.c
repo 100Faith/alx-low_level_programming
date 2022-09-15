be670dc64ecb73d259d725285b252b45a198a77b
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-15 10:25:18 -0700

    Task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: input number one
+ * @b: input number two
+ *
+ * Return: result of the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
