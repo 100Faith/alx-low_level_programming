83762a082489267958e63531c6dcc87e98426022
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-15 13:16:12 -0700

    Task 7 commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line 
+ *
+ * @n: number of times a diagonal line is printed
+ *
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
