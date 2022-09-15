45761c9e3a4685c5c5e94328acff38d72791a053
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-15 10:12:05 -0700

    Task 1 commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: input number
+ *
+ * Return: 1 if the digit is between 0 to 9 and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
