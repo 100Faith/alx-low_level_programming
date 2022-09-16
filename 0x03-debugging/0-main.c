3a0c8b41eae22e7edb11484bc6ec0cf5480058ed
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-16 04:16:20 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - test positive_or_negative function
  *
@@ -6,10 +7,10 @@
  */
 int main(void)
 {
-	int a;
+	int i;
 
-	a = 0;
-	positive_or_negative(a);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
