0e67b8431b82ead570f151f00b820c4f7edcc151
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-19 09:23:03 -0700

    Prototype

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: string to be printed
+ *
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar ('\n');
+}
+
