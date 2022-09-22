e20fb16a6030446e1055b22ce8db49aa3c1e9c8e
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-22 14:18:17 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  _strcmp - compares two strings
+ *  @s1: first string
+ *  @s2: second string
+ *
+ *  Return: the difference between the two srings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int first;
+	int second;
+
+	first = 0;
+	second = 0;
+	while ((s1[first] != '\0' || s2[first] != '\0') && second == 0)
+	{
+		if (s1[first] != s2[second])
+	{
+		second = s1[first] - s2[first];
+	}
+		first++;
+	}
+	return (second);
+}
