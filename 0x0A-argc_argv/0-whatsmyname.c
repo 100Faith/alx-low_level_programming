5fe0c37fa6bc911f985c03fc069f55f47d4b78a3
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-30 14:01:00 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the program name.
+ * @argc: number of command line arguments.
+ * @argv: contains the program command line arguments.
+ *
+ * Return: Always 0, success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s \n", argv[0]);
+		i++;
+	}
+	return (0);
+}
