677962e2cc075e57b7aa6bcdd9673afd6302e31e
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-30 14:20:58 -0700

    My name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,16 @@
  * @argc: number of command line arguments.
  * @argv: contains the program command line arguments.
  *
- * Return: Always 0, success
+ * Return: 0
  */
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
