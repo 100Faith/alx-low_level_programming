089b86dd5941e12dcefba22f02ecfa34306444b5
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-30 16:09:41 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: Size of array argv
+ * @argv: array pointer to string.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
