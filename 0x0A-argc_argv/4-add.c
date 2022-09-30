440a05d097ae8c85f688588f9eaaa92e335c6a0d
Faith_Wasonga <faithwasonga@gmail.com>
2022-09-30 16:54:35 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - adds positive numbers.
+ *
+ * @argc: Size of argv
+ * @argv: Array pointer.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int add = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
